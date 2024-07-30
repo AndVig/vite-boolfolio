d08b3ddc2a8687f64249808b39932d29562ab0f1
AndVig <andrea.vigna2@gmail.com>
2024-07-30 21:07:00 +0200

    [UPD] Seguo lezione, fino a fontawesome

--- src/main.js
+++ src/main.js
@@ -1,5 +1,30 @@
 import { createApp } from 'vue'
-import './style.scss'
+// Import our custom CSS
+import './scss/style.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+import "@fortawesome/fontawesome-free"
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faUserSecret)
+
+// createApp(App)
+// .component('font-awesome-icon', FontAwesomeIcon)
+// .mount('#app')
+
+
+
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
