3026502c686f1884a980ec0a95568eb02aedff91
AndVig <andrea.vigna2@gmail.com>
2024-07-30 22:48:40 +0200

    [UPD] Arrivato alla paginazione

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,52 @@
-<script setup>
+<script>
 
+import axios from 'axios';
+export default {
+  name:'Projects',
+  data(){
+    return{
+      'title':'Projects',
+      'projects':[],
+    }
+  },
+  methods:{
+    testLifeCycle(){
+      console.log('prova ciclo vita Vue')
+    },
+    getProjects(){
+      const result =axios
+      .get('http://127.0.0.1:8000/api/projects')
+      .then((response)=>{console.log(response);
+        console.log('fin qui tutto bene');
+        console.log(response.data.results);
+
+        this.projects=response.data.results
+
+
+      })
+      .catch(error=>console.log(error));
+    }
+  },
+  created(){
+    this.testLifeCycle();
+    this.getProjects();
+  }
+};
 </script>
 
 <template>
-  <div class="container">
-
-    <h1>app</h1>
-    <div class="row">
-      <div class="col"><font-awesome-icon :icon="['fas','user-secret']"/></div>
-      <div class="col">Colonna</div>
-      <div class="col">Colonna</div>
-      <div class="col">Colonna</div>
-      <div class="col">Colonna</div>
-      <div class="col">Colonna</div>
   
-    </div>
-  </div>
+    <header>header</header>
+    <main>
+      <div class="container">
+        <h1>{{title}}</h1>
+
+      </div>
+
+    </main>
+    <footer>footer</footer>
+    
+  
 </template>
 
 <style lang="scss" scoped>
